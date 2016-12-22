cb26a2018eb7baf64a6d1c25002470e5c1a4e47a
Fernando Gouvea <fernandosrg1@gmail.com>
2016-12-22 02:25:06 -0200

    Fazendo script para juntar os tweets de diferentes datasets e futuramente executar pré-processamento.

---
+++ preprocessing.py
@@ -0,0 +1,41 @@
+import os
+
+count = 0
+
+def preprocess_tweet(tweet):
+  global count
+
+  count += 1
+  return tweet
+
+def preprocess_file(input_file, output_file):
+  tweet = ''
+  for line in input_file:
+    if line[:4] == '####':
+      if tweet[:4] == '###!':
+        processed_tweet = preprocess_tweet(tweet)
+        output_file.write(processed_tweet)
+        output_file.write('#####\n')
+      tweet = ''
+    else:
+      tweet += line
+
+path_of_file = os.path.dirname(os.path.realpath(__file__))
+path_datasets = path_of_file + "/data_classified"
+
+output_folder = path_of_file + "/bin"
+output_file_path = output_folder + "/dataset.txt"
+
+if not (os.path.isdir(output_folder)):
+  os.mkdir(output_folder)
+
+output_file = open(output_file_path, 'w')
+
+for input_file_name in os.listdir(path_datasets):
+  input_file = open("{}/{}".format(path_datasets, input_file_name), 'r')
+  preprocess_file(input_file, output_file)
+  input_file.close()
+
+output_file.close()
+
+print count
