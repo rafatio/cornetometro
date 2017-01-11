26853de2e17d2cea90541ab5b85ab7a4b0a65370
rafatio <rafael.rafatio@gmail.com>
2017-01-11 13:08:44 +0000

    Alterando o random state das features de teste. 85.25%

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -45,12 +45,12 @@ positive_tweets, negative_tweets = get_classified_tweets()
 positive_features = get_features(tokenize_tweets(positive_tweets), "positive")
 negative_features = get_features(tokenize_tweets(negative_tweets), "negative")
 
-train_features, test_features = train_test_split(negative_features + positive_features, test_size=0.25, random_state=20)
+train_features, test_features = train_test_split(negative_features + positive_features, test_size=0.25, random_state=40)
 
 print 'train on %d instances, test on %d instances' % (len(train_features), len(test_features))
 
 classifier = NaiveBayesClassifier.train(train_features)
 
 print 'accuracy:', nltk.classify.accuracy(classifier, test_features)
-print classifier.show_most_informative_features()
+classifier.show_most_informative_features()
 #print classifier.classify(word_feats(nltk.word_tokenize("Benzema you fat shit, should be sold")))
