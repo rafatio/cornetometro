f99bdec24669e60204f554ae1904d7d4831f151d
Fernando Gouvea <fernandosrg1@gmail.com>
2017-01-11 18:56:24 -0200

    Guardando nomes dos times e dos jogadores usados ao coletar tweets.

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -7,8 +7,12 @@ import json
 import time
 
 class CustomListener(StreamListener):
-    def __init__(self):
+    def __init__(self, teams, players):
         self.tweets_json_file = open("data/twitter_data_" + time.strftime("%H:%M:%S_%d-%m"), "w")
+        self.tweets_json_file.write(' '.join(teams))
+        self.tweets_json_file.write('\n')
+        self.tweets_json_file.write(' '.join(players))
+        self.tweets_json_file.write('\n')
 
     def on_data(self, data):
         tweet_json = json.loads(data)
@@ -21,13 +25,16 @@ class CustomListener(StreamListener):
         print status
 
 
-if __name__ == '__main__':
-    listener = CustomListener()
+if __name__ == '__main__': 
+    teams = ['Barcelona', 'Barca', 'Athletic', 'Bilbao']
+    players = ['Cillessen', 'Sergi', 'Roberto', 'Pique', 'Umtiti', 'Jordi', 'Alba', 'Busquets', 'Iniesta', 'Rafinha', 'Alcantara', 'Messi', 'Neymar', 'Suarez', 'Iraizoz',  'Boveda', 'Etxeita', 'Laporte', 'Balenziaga', 'San Jose', 'Benat', 'Eraso', 'Inaki', 'Williams', 'Saborit', 'Sabin', 'Merino']
+
+    listener = CustomListener(teams, players)
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, listener)
-    players_list = ['stegen', 'pique', 'rakitic', 'suarez', 'messi', 'neymar', 'mascherano', 'alba', 'navas', 'carvajal', 'sergio ramos', 'varane', 'cristiano ronaldo', 'cr7', 'benzema', 'marcelo', 'kovacic', 'vazquez', 'modric', 'isco']
 
+    #players_list = ['stegen', 'pique', 'rakitic', 'suarez', 'messi', 'neymar', 'mascherano', 'alba', 'navas', 'carvajal', 'sergio ramos', 'varane', 'cristiano ronaldo', 'cr7', 'benzema', 'marcelo', 'kovacic', 'vazquez', 'modric', 'isco']
 
     #stream.filter(languages=['en'], track=['bravo', 'fernandinho', 'gundogan', 'navas', 'silva', 'de bruyne', 'aguero', 'curtois', 'kolarov', 'cahill', 'kante', 'fabregas', 'pedro', 'costa', 'hazard'])
-    stream.filter(languages=['en'], track=players_list)
+    stream.filter(languages=['en'], track=players)
