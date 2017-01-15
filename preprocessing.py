ca9ffc0792b4726e086f0a02f6113224e40e8987
Fernando Gouvea <fernandosrg1@gmail.com>
2017-01-15 00:16:43 -0200

    Corrigindo bug na substituição do nome de times e jogadores; Colocando main em um método

--- preprocessing.py
+++ preprocessing.py
@@ -17,13 +17,13 @@ def preprocess_tweet(tweet, teams, players):
 
   # Replaces team names for custom token
   for team in teams:
-    if team in processed_tweet:
-      processed_tweet = processed_tweet.replace(team, "#team")
+    if team.lower() in processed_tweet:
+      processed_tweet = processed_tweet.replace(team.lower(), "#team")
 
   # Replaces player names for custom token
   for player in players:
-    if player in processed_tweet:
-      processed_tweet = processed_tweet.replace(player, "#player")
+    if player.lower() in processed_tweet:
+      processed_tweet = processed_tweet.replace(player.lower(), "#player")
 
   # Merge two consecutive occurences of team and player tokens
   processed_tweet = processed_tweet.replace("#team #team", "#team")
@@ -48,22 +48,26 @@ def preprocess_file(input_file, output_file):
     else:
       tweet += line
 
-path_of_file = os.path.dirname(os.path.realpath(__file__))
-path_datasets = path_of_file + "/data_classified"
+def main():
+    path_of_file = os.path.dirname(os.path.realpath(__file__))
+    path_datasets = path_of_file + "/data_classified"
 
-output_folder = path_of_file + "/preprocessed"
-output_file_path = output_folder + "/dataset.txt"
+    output_folder = path_of_file + "/preprocessed"
+    output_file_path = output_folder + "/dataset.txt"
 
-if not (os.path.isdir(output_folder)):
-  os.mkdir(output_folder)
+    if not (os.path.isdir(output_folder)):
+      os.mkdir(output_folder)
 
-output_file = open(output_file_path, 'w')
+    output_file = open(output_file_path, 'w')
 
-for input_file_name in os.listdir(path_datasets):
-  input_file = open("{}/{}".format(path_datasets, input_file_name), 'r')
-  preprocess_file(input_file, output_file)
-  input_file.close()
+    for input_file_name in os.listdir(path_datasets):
+      input_file = open("{}/{}".format(path_datasets, input_file_name), 'r')
+      preprocess_file(input_file, output_file)
+      input_file.close()
 
-output_file.close()
+    output_file.close()
 
-print count
+    print count
+
+if __name__ == "__main__":
+    main()
